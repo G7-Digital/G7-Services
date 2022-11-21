6f2523e9aaee9e8aebb393ca5703d1e5b7166d2e
Danieli Jaqueline do Nascimento <djaqueline.nascimento@gmail.com>
2022-11-21 12:02:51 -0300

    adiciona validações no formulário adicionar serviços

--- Public/adicionar-produtos/add-produto.js
+++ Public/adicionar-produtos/add-produto.js
@@ -1,3 +1,4 @@
+window.addEventListener('load', () => {
 //Menu Hamburgur e Menu Usuário
 //mostra e esconde o menu hamburguer e usuario//
 const menuHamburguer = document.querySelector(".icone-menu");
@@ -19,3 +20,78 @@ menuUsuario.onclick = function usuarioActive() {
     usuario.style.display = "block";
   }
 }
+
+//                 Validações de Input
+//Input Categoria
+let inputCategoria = document.getElementById('choose-cat')
+
+const mensagemCat = document.createElement('span');
+mensagemCat.innerText = 'Campo Obrigatório!';
+mensagemCat.classList.add('erro1')
+
+inputCategoria.addEventListener ('blur', function(event) {
+ 
+  event.preventDefault();
+
+  if (inputCategoria.value != "") {
+    return
+  } else {    
+    inputCategoria.insertAdjacentElement('afterend', mensagemCat);
+  }
+})
+
+//input Código 
+let inputCodigo = document.getElementById('codigo')
+
+const mensagemCod = document.createElement('span');
+mensagemCod.innerText = 'Insira um código com mínimo 4 dígitos';
+mensagemCod.classList.add('erro1')
+
+inputCodigo.addEventListener ('blur', function(event) {
+ 
+  event.preventDefault();
+
+  if (inputCodigo.value.length >= 4) {
+    return
+  } else {    
+    inputCodigo.insertAdjacentElement('afterend', mensagemCod);
+  }
+})
+
+//Input Nome do serviço 
+let inputServ = document.getElementById('name-service')
+
+const mensagemServ = document.createElement('span');
+mensagemServ.innerText = 'Insira um nome com 2 a 20 dígitos!';
+mensagemServ.classList.add('erro1');
+
+inputServ.addEventListener ('blur', function(event) {
+ 
+  event.preventDefault();
+
+  if (inputServ.value.length >= 2 && inputServ.value.length <= 20) {
+    return
+  } else {    
+    inputServ.insertAdjacentElement('afterend', mensagemServ);
+  }
+})
+
+//Input Preço
+let inputPrice = document.getElementById('price'); 
+
+const mensagemPrice = document.createElement('span');
+mensagemPrice.innerText = 'Campo Obrigatório!';
+mensagemPrice.classList.add('erro1');
+
+inputPrice.addEventListener('blur', function(event) {
+  event.preventDefault();
+
+  if (inputPrice.value != "") {
+    return
+  } else {    
+    inputPrice.insertAdjacentElement('afterend', mensagemPrice);
+  }
+})
+
+})
+
